ce0b8a48fd505e174d83c669881e41e3a813cea6
Michael Lee <vlmlee2@gmail.com>
2017-01-08 21:40:06 -0900

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,32 @@
+/*
+
+Problem: 
+Determine if a string is a palindrome.
+
+Solution:
+A string is a palindrome if it is the same reversed. So, what we 
+need to do is reverse the string and see if it's equal to itself. 
+
+*/
+
+const assert = require('assert');
+
+// Using built-in methods 'split', 'reverse', and 'join'
+// a.k.a. the *best* way
+function palindrome(str) {
+	return (str.split('').reverse().join('') === str) ? true : false;
+}
+
+// 'In-place'-like (since strings are immutable) iterative solution
+function inPlacePalindrome(str) {
+	let p = '';
+	for (let i = str.length - 1; i >= 0; i--) {
+		p += str[i];
+	}
+	return p === str;
+}
+
+assert(palindrome('abcba'));
+assert.equal(palindrome('abc'), false);
+assert(inPlacePalindrome('abbba'));
+assert.equal(inPlacePalindrome('abc'), false);
