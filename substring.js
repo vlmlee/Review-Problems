cfa61dca21bc063dd81dad27b092027d3d231a0e
Michael Lee <vlmlee2@gmail.com>
2017-01-08 22:29:31 -0900

    Create substring.js

---
+++ substring.js
@@ -0,0 +1,28 @@
+/*
+
+Problem:
+Determine if a string is a substring and if found, print out the index.
+
+Solution:
+We can use es7's include to quickly determine if a substring exist and then
+do a linear search of each substring of the same length in the main
+string.
+
+*/
+
+const assert = require('assert');
+
+function isSubstring(str, sub) {
+	if (str.includes(sub)) {
+		for (let i = 0, len = sub.length; i < str.length; i++) {
+			if (str.slice(i, len + i) === sub) {
+				return `Index found: ${i}`;
+			}
+		}
+	} else {
+		return false;
+	}
+}
+
+assert.equal(isSubstring('supersubstring', 'str'), 'Index found: 8');
+assert.equal(isSubstring('notinthisone', 'str'), false);
