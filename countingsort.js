4c5514ff27bbbb7e8929d128a167d38594291e43
Michael Lee <vlmlee2@gmail.com>
2017-01-10 02:12:18 -0900

    Update countingsort.js

--- countingsort.js
+++ countingsort.js
@@ -2,12 +2,9 @@ const assert = require('assert');
 
 function countingsort(arr) {
 	let count = {},
-	    result = [];
+		result = [];
 
 	arr.map(i => {
-		if (typeof i === 'string') {
-			i = i.charCodeAt(0);
-		}
 		if (count[i]) {
 			count[i]++;
 		} else {
@@ -17,11 +14,6 @@ function countingsort(arr) {
 
 	Object.keys(count).map(i => {
 		for (let j = 0; j < count[i]; j++) {
-			if (~~i >= 97) {
-				i = String.fromCharCode(i);
-			} else {
-				i = ~~i;
-			}
 			result.push(i);
 		}
 	});
@@ -29,4 +21,3 @@ function countingsort(arr) {
 }
 
 assert.deepEqual(countingsort([4, 2, 3, 1, 1, 2]), [1, 1, 2, 2, 3, 4]);
-assert.deepEqual(countingsort(['d', 'a','f', 'c', 'b']), ['a', 'b', 'c', 'd', 'f']);
