8a777ff5279a1236b52bc7473e123d54d35d0bb1
Michael Lee <vlmlee2@gmail.com>
2017-01-10 20:25:25 -0900

    Update BST.js

--- BST.js
+++ BST.js
@@ -109,6 +109,7 @@ class BST {
 		if (!preorder) {
 			preorder = [];
 		}
+
 		if (node) {
 			preorder.push(node.data);
 			this.preOrder(node.left, preorder);
@@ -150,17 +151,47 @@ class BST {
 		}
 		return node;
 	}
+
+	dfs(current, order) {
+		if (!order) {
+			order = [];
+		}
+		return this.preOrder(current, order);
+	}
+
+	bfs(current, order) {
+		if (!order) {
+			order = [];
+		}
+		this.queue = [];
+		this.queue.push(current);
+		while(this.queue.length) {
+			let node = this.queue.shift();
+			order.push(node.data);
+			if (node.left) {
+				this.queue.push(node.left);
+			}
+			if (node.right) {
+				this.queue.push(node.right);
+			}
+		}
+		return order;
+	}
+}
+
+function arrayToBST(arr, tree) {
+	if (!arr.length) {
+		return;
+	}
+	let mid = Math.floor((arr.length)/ 2);
+	tree.add(arr[mid]);
+	arrayToBST(arr.slice(0, mid), tree);
+	arrayToBST(arr.slice(mid+1), tree);
 }
 
 let tree = Object.create(BST.prototype);
-
-tree.add(2);
-tree.add(1);
-tree.add(3);
-tree.add(4);
-tree.add(5);
-
-assert.deepEqual(tree.preOrder(tree.root), [2, 1, 3, 4, 5]);
-assert.deepEqual(tree.inOrder(tree.root), [1, 2, 3, 4, 5]);
-assert.deepEqual(tree.postOrder(tree.root), [1, 5, 4, 3, 2]);
-
+arrayToBST([1, 2, 3, 4, 5, 6, 7], tree);
+assert.deepEqual(tree.preOrder(tree.root), [4, 2, 1, 3, 6, 5, 7]);
+assert.deepEqual(tree.inOrder(tree.root), [1, 2, 3, 4, 5, 6, 7]);
+assert.deepEqual(tree.postOrder(tree.root), [1, 3, 2, 5, 7, 6, 4]);
+assert.deepEqual(tree.bfs(tree.root), [4, 2, 6, 1, 3, 5, 7]);
