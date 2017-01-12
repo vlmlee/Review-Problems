07f7cd15f6ea083a97b50f5f3812ee2749b54b70
Michael Lee <vlmlee2@gmail.com>
2017-01-12 00:50:23 -0900

    Update dfs.js

--- dfs.js
+++ dfs.js
@@ -1,3 +1,21 @@
+/*
+
+Problem:
+Implement depth-first search.
+
+Solution:
+Depth first search is essentially preorder traversal,
+meaning that it we want to print out all the leaves of
+a subtree before their parents.
+
+*/
+
+/*
+* Depth first search
+* @param {Node} current - Starting point in the subtree.
+* @order {Array} order - An array containing any previous values.
+* @returns {Array} - An array of values in preorder order.
+*/
 dfs(current, order) {
   if (!order) {
     order = [];
