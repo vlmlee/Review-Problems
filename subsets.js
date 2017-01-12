e802164e129d79c6c24761ffafff27b5a907d134
Michael Lee <vlmlee2@gmail.com>
2017-01-12 09:11:52 -0900

    Update subsets.js

--- subsets.js
+++ subsets.js
@@ -1,3 +1,45 @@
+/*
+
+Problem:
+Find all subsets of an array.
+
+Solution:
+This solution uses the concept of a bitmask to filter the
+element in the ith position while the other elements are 
+ignored.
+
+Without the bitmask, to prevent duplicate arrays, we would have 
+an array like:
+
+[a, b, c, d] => [ [a], [a, b], [a, b, c], [a, b, c, d],
+                  [b], [b, c], [b, c, d], [c], [c, d], [d]
+                  ... ]
+
+And collecting any elements before the ith position would be 
+inefficient.
+
+With (i & Math.pow(2, j)), when i = 1, i bitwise would be 0001 (in four places). 
+Then when j = 0 (0001), only the first element will be pushed into the superset, 
+with all the others filtered. On j = 1 (0010), no elements are added. On 
+j = 2 (0100), (i & Math.pow(2, j)) is false (0001 & 0100). On j = 3, (0001 & 1001) 
+is true and so arr[3] is added. 
+
+Thus, when i = 1, [a, d] are added to the superset. 
+When i = 2, [b] is added to the superset.
+When i = 3, [a, b, d] is added to the superset.
+When i = 4, [c]  is added to the superset.
+...
+
+It is the case that we can get all combinations this way.
+
+*/
+
+
+/**
+* Returns the subsets of an array.
+* @param {Array} arr - An input array.
+* @returns {Array} set - Returns the superset of the array.
+*/
 function subsets(arr) {
 	let set = [],
 		combinationsCount = Math.pow(2, arr.length);
