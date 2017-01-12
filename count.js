e0570cd5f39b9adf777d1945931f6ef6cce4071d
Michael Lee <vlmlee2@gmail.com>
2017-01-11 23:22:46 -0900

    Update count.js

--- count.js
+++ count.js
@@ -1,8 +1,27 @@
+/*
+
+Problem:
+Collect all occurrences of an item in a list.
+
+Solution:
+We create an object to tally the occurrences of each 
+element. We do so by simply incrementing the value by 1
+if it exists in the object or setting the value to 1 if 
+it doesn't already exist.
+
+*/
+
 const assert = require('assert');
 
+/**
+* Counting function
+* @param {Array} arr - Input array to count items in the list.
+* @returns {Object} col - Returns an object showing the number of
+* occurrences of each item.
+*/ 
 function count(arr) {
 	let col = {};
-	arr.forEach(i => {
+	arr.map(i => {
 		if (col[i]) {
 			col[i] += 1;
 		} else {
