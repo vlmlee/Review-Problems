62df08a797bf4d3995149623b6f8e2b92de6cd38
Michael Lee <vlmlee2@gmail.com>
2017-01-12 06:38:17 -0900

    Update steps.js

--- steps.js
+++ steps.js
@@ -1,6 +1,31 @@
+/*
+
+Problem:
+Given you can climb 1, 2, or 3 stairs in one step, how many ways can you reach the top?
+
+Solution:
+You can solve the problem inductively. The number of ways you can climb 1 step is 1
+since you can only use 1 step. With 2 steps, the number of ways is 2, that is, the 
+number of ways you can climb using single steps and double steps. With 3 steps, it 
+becomes the sum of the number of ways you can climb it using 1, 2, or 3 steps. This
+has the characteristics of recursion and has the recurrence:
+
+f(n) = f(n-1) + f(n-2) + f(n-3)
+
+which is similiar to the fibonacci sequence. So, we can solve this problem
+by applying the algorithm for a fibonacci sequence.
+
+*/
+
+
 const assert = require('assert');
 let cache = {};
 
+/**
+* Number of ways to reach to top of a n staircase with 1, 2, or 3 possible step increments.
+* @param {Number} n - The number of steps.
+* @returns {Number} value - The number ways to get to the top of staircase.
+*/
 function steps(n) {
 	if (cache[n]) {
 		return cache[n];
